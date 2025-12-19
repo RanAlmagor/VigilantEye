934a94577cb1d06e85d62c171fdca45b864e885b
RanAlmagor <ranalmagor100@gmail.com>
2025-12-19 17:02:21 +0200

    System controller header

--- include/Core/SystemController.h
+++ include/Core/SystemController.h
@@ -1,1 +1,38 @@
-#pragma once
+// ===============================
+// SystemController.h
+// ===============================
+
+#ifndef CORE_SYSTEM_CONTROLLER_H
+#define CORE_SYSTEM_CONTROLLER_H
+
+#include <memory>  
+#include <atomic>  
+#include "HAL/Sensors/IFrameSource.h"
+#include "Core/Pipeline.h"
+
+class SystemController
+{
+public:
+
+    /**
+     * @brief Constructor
+     * @param sensor The input source (Webcam or File), ownership is transferred here.
+     */
+    explicit SystemController(std::unique_ptr<IFrameSource> sensor);
+
+    /**
+     * @brief Main loop of the application.
+     * Runs until ESC is pressed or stream ends.
+     */
+    void run();
+
+private:
+  
+    std::unique_ptr<IFrameSource> m_sensor;
+    Pipeline m_pipeline;
+
+    // Control flag
+    std::atomic<bool> m_running{ false };
+};
+
+#endif // CORE_SYSTEM_CONTROLLER_H
